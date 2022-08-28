fcd31ce0d8047960059967bde7a0d94d46d07f3a
AngerOverApathy <violasian@gmail.com>
2022-08-28 13:42:28 -0700

    codewars challenge 1

--- script.js
+++ script.js
@@ -2,13 +2,26 @@
 //grab just the initials from a full name
 //i.e Andrew Jackson should print as A.J.
 
-function abbrevName(name){
-    //take the first letter of each word
-    //return initials with period
-    let fullName = name.split(' ')
-    return (fullName[0][0] + '.' + fullName[1][0] + '.').toUpperCase()
-  }
+// function abbrevName(name){
+//     //take the first letter of each word
+//     //return initials with period
+//     let fullName = name.split(' ')
+//     return (fullName[0][0] + '.' + fullName[1][0] + '.').toUpperCase()
+//   }
 
-console.log(abbrevName('Andrew Jackson'), 'A.J.')
-console.log(abbrevName('Julius Ceasar'), 'J.C.')
-console.log(abbrevName('Harley Quinn'), 'H.Q.')
+// console.log(abbrevName('Andrew Jackson'), 'A.J.')
+// console.log(abbrevName('Julius Ceasar'), 'J.C.')
+// console.log(abbrevName('Harley Quinn'), 'H.Q.')
+
+//    ~CHALLENGE 2~
+// create a function that takes in 4 numbers and adds the first 3, and divides by the 4th
+//return the result
+
+function averageThreeNumbers (num1, num2, num3, num4) {
+    let sum = num1 + num2 + num3
+    let result = sum / num4
+
+    return result
+}
+
+console.log(averageThreeNumbers(2, 6, 4, 2))
