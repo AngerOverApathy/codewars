0b4917614d8126b460bf639d33270d7459d8fbe5
AngerOverApathy <violasian@gmail.com>
2022-08-29 13:46:41 -0700

    codewars challenge 2

--- script.js
+++ script.js
@@ -17,11 +17,22 @@
 // create a function that takes in 4 numbers and adds the first 3, and divides by the 4th
 //return the result
 
-function averageThreeNumbers (num1, num2, num3, num4) {
-    let sum = num1 + num2 + num3
-    let result = sum / num4
+// function averageThreeNumbers (num1, num2, num3, num4) {
+//     let sum = num1 + num2 + num3
+//     let result = sum / num4
 
+//     return result
+// }
+
+// console.log(averageThreeNumbers(2, 6, 4, 2))
+
+
+
+//    ~CHALLENGE 3
+// create a function that takes in 2 numbers, console log first number to power of second
+function exponents(num1, num2) {
+    let result = num1 ** num2
     return result
 }
 
-console.log(averageThreeNumbers(2, 6, 4, 2))
+console.log(exponents(4, 2))
