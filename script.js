1bceca0f0cffefcc90559f1c65981ef8171660da
AngerOverApathy <violasian@gmail.com>
2022-08-31 17:11:12 -0700

    codewars challenge 2

--- script.js
+++ script.js
@@ -34,21 +34,46 @@
 // function exponents(num1, num2) {
 //     let result = num1 ** num2
 //     return result
+// }             
+
+//            OR
+
+//function exponents(num1, num2) {
+//     console.log(Math.pow(num1, num2))
 // }
+
 
 // console.log(exponents(4, 2))
 
 //     ~CHALLENGE4~
-//create a conditional that takes a boolean and a string
-//if the boolean is true, alert string, if false, console log string
+// create a conditional that takes a boolean and a string
+// if the boolean is true, alert string, if false, console log string
 
-function alertTrue(word) {
-    if (word === 'yes') {
-        alert(word, 'TRUE!')
-    } else {
-        console.log(word, 'false')
-    }
+function alertTrue(b, str) {
+    b ? alert(true) : console.log(str)
 }
+console.log(alertTrue(true, 'i am true'))
+console.log(alertTrue(false, 'i am false'))
 
-console.log(alertTrue('yes'), 'true')
-console.log(alertTrue('boo'), 'false')
+
+//        ~CHALLENGE 5~
+//create a function that takes in a number, console all values from 1 to that number
+//if the number is divisible by 3, log fizz instead of that number
+//if the number is divisible by 5 log buzz instead of the number
+//if the number is divisible by 3 and 5, log fizzbuzz instead of that number
+
+// function fizzBuzz(count, num) {
+//     for (count = 0; count < num; count++) {
+//         if (count % 3 === 0 && num % 5 ===0 ) {
+//             console.log(count + 'fizzbuzz')
+//         } else if (count % 3 === 0) {
+//             console.log(count + 'fizz')
+//         } else if (count % 5 === 0) {
+//             console.log(count, 'buzz')
+//         } else {
+//             console.log(count)
+//         }
+//     }
+// }
+
+// console.log(fizzBuzz(1, 700))
