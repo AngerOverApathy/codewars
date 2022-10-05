460ad5df2bb0e3cf3a84768a5823a92b9158ed8d
AngerOverApathy <violasian@gmail.com>
2022-10-04 22:07:50 -0700

    codewars challenge

--- script.js
+++ script.js
@@ -27,17 +27,17 @@
 // }
 // summertime('July')
 
-function skipFive(num) {
-    for (let i=0; i < num; i++) {
-        if (i % 5 === 0) {
-            console.log('')
-        } else {
-            console.log(i)
-        }
-    }
-}
+// function skipFive(num) {
+//     for (let i=0; i < num; i++) {
+//         if (i % 5 === 0) {
+//             console.log('')
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
 
-skipFive(850)
+// skipFive(850)
 
 
 //////////////////////////////////////////////////////////////////////////
