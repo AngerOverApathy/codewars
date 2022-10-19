eb95a8e0b4e312de7efc21c4a8fcc777f6af9b5a
AngerOverApathy <violasian@gmail.com>
2022-10-19 11:27:45 -0700

    codewars challenge

--- script.js
+++ script.js
@@ -216,11 +216,18 @@
 
 //create a function that takes in an array of numbers
 //return a new array of every even number
-function returnEvenNums(arr) {
-    let evenNums = arr.filter(num => num % 2 ==0 )
-    return evenNums
-}
 let array = [1,2,3,4,5,6,7,8,9,10]
-console.log(returnEvenNums(array))
+
+// function returnEvenNums(arr) {
+//     let evenNums = arr.filter(num => num % 2 ==0 )
+//     return evenNums
+// }
+// console.log(returnEvenNums(array))
+//OR
+function returnEven(){
+    for (let i=0;i<arr.length;i++){
+
+    }
+}
 
 
