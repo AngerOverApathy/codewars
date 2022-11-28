b5f5fefabfedbc090222516e06f9a78aa48ce2a9
AngerOverApathy <violasian@gmail.com>
2022-11-28 09:17:33 -0800

    codewars challenge

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-let numArr = [10,20,30,40,50]
-function sum(arr){
-  let total = 0
-  arr.forEach(arr => total += arr)
-  console.log(total)
-}
+// let numArr = [10,20,30,40,50]
+// function sum(arr){
+//   let total = 0
+//   arr.forEach(arr => total += arr)
+//   console.log(total)
+// }
+// sum(numArr)
 
-sum(numArr)
