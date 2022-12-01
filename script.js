96aefe7ee41ff348fb8985028cdb3246eddccf2a
AngerOverApathy <violasian@gmail.com>
2022-12-01 10:40:27 -0800

    codewars challenge

--- script.js
+++ script.js
@@ -14,3 +14,10 @@
 //   }
 // }
 // squareNum(numArr)
+
+let string="I am a string"
+function reverseString(str){
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseString(string))
