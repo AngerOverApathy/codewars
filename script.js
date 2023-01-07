cc5c2a298c5fc1ec93d51b89f63e9e3ccba00022
AngerOverApathy <violasian@gmail.com>
2023-01-06 19:55:54 -0800

    codewars challenge

--- script.js
+++ script.js
@@ -68,12 +68,3 @@
 //     alert('healing')
 //   }
 // }
-
-let nums = [2,4,6,8,10]
-function squareNUms(arr){
-  arr.forEach((x,i)=>{
-    arr[i] = Math.pow(x,2)
-  })
-  return arr
-}
-console.log(squareNUms(nums))
