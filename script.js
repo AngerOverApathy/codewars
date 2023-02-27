a8e66a4390752d0785f6ab02a80e7e3b00752dfb
AngerOverApathy <violasian@gmail.com>
2023-02-27 12:58:56 -0800

    codewars challenge

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ pizza.firstTopping = 'cheese'
 pizza.secondTopping = 'pepperoni'
 pizza.seasoning = 'italian'
 pizza.ready = true
+
+pizza.burn = function(){
+    console.log("I'm gonna burn your mouth!")
+}
