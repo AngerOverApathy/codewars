0efa7e7b3a218996720180ee771f6f1979a24c11
AngerOverApathy <violasian@gmail.com>
2023-03-04 13:28:03 -0800

    codewars challenge

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let pizza = {}
+// let pizza = {}
 
-pizza.firstTopping = 'cheese'
-pizza.secondTopping = 'pepperoni'
-pizza.seasoning = 'italian'
-pizza.ready = true
+// pizza.firstTopping = 'cheese'
+// pizza.secondTopping = 'pepperoni'
+// pizza.seasoning = 'italian'
+// pizza.ready = true
 
-pizza.burn = function(){
-    console.log("I'm gonna burn your mouth!")
-}
+// pizza.burn = function(){
+//     console.log("I'm gonna burn your mouth!")
+// }
 
-pizza.consume = function(){
-    console.log("You're eating me!")
-}
+// pizza.consume = function(){
+//     console.log("You're eating me!")
+// }
 
-pizza.description = function(){
-    console.log(`I am topped with ${pizza.firstTopping}, ${pizza.secondTopping}, and ${pizza.seasoning}`)
-}
+// pizza.description = function(){
+//     console.log(`I am topped with ${pizza.firstTopping}, ${pizza.secondTopping}, and ${pizza.seasoning}`)
+// }
