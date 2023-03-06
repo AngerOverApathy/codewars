2cb4a741c7ac39fe7941e6b257fce82481bcddf0
AngerOverApathy <violasian@gmail.com>
2023-03-06 13:28:37 -0800

    codewars challenge

--- app.js
+++ app.js
@@ -478,3 +478,22 @@
 //     alert("I'm speeding up!")
 //   }
 // }
+
+// let pizza = {}
+
+// pizza.firstTopping = 'cheese'
+// pizza.secondTopping = 'pepperoni'
+// pizza.seasoning = 'italian'
+// pizza.ready = true
+
+// pizza.burn = function(){
+//     console.log("I'm gonna burn your mouth!")
+// }
+
+// pizza.consume = function(){
+//     console.log("You're eating me!")
+// }
+
+// pizza.description = function(){
+//     console.log(`I am topped with ${pizza.firstTopping}, ${pizza.secondTopping}, and ${pizza.seasoning}`)
+// }
