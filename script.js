29bad727f4a3a009497b370684e5986b26d2aaa3
AngerOverApathy <violasian@gmail.com>
2023-03-09 13:32:59 -0800

    codewars challenge

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function MakePizza(pizzaToppings, pizzaCrust, pizzaSeasoning, pizzaReady){
-
+    this.toppings = pizzaToppings
+    this.crust = pizzaCrust
+    this.seasoning = pizzaSeasoning
+    this.ready = pizzaReady
+    this.burn = function(){
+        console.log("I'm burning your mouth!")
+    }
+    this.deliveryTime = function(){
+        console.log('Your pizza is ready!')
+    }
+    this.consume = function(){
+        console.log("You're eating me!")
+    }
 }
