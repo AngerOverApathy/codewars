23d65eba823a7ad47aa021246af0c32ee87e94ea
AngerOverApathy <violasian@gmail.com>
2023-03-15 15:36:10 -0700

    codewars challenge

--- class26-materials/objects-dog-photos/js/main.js
+++ class26-materials/objects-dog-photos/js/main.js
@@ -4,6 +4,7 @@ fetch("https://dog.ceo/api/breeds/image/random")
     .then(res => res.json())
     .then(data => {
         console.log(data.message)
+        document.querySelector('img').src = data.message
     })
     .catch(err => {
         console.log(`error ${err}`)
