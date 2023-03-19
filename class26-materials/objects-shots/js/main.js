f205f6bd4479b72b9cb1fdbccddfd3905fa7c24c
AngerOverApathy <violasian@gmail.com>
2023-03-19 16:10:05 -0700

    codewars challenge

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -1,5 +1,9 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+
+document.querySelector('button').addEventListener('click', getDrink)
+
+function getDrink(){
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
     .then(res => res.json())
     .then(data => {
         console.log(data.drinks[0])
@@ -10,3 +14,5 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
     .catch(err => {
         console.log(`error ${err}`)
     })
+}
+
