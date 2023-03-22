16c55e19411086e65556e850de8473bdcfb73a91
AngerOverApathy <violasian@gmail.com>
2023-03-22 07:45:02 -0700

    codewars challenge

--- class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,5 +1,19 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
-class
-
-
+class NetflixShow{
+    constructor(showName, showTheme, showLength, showSong){
+        this.name = showName
+        this.theme = showTheme
+        this.length = showLength
+        this.song = showSong
+    }
+    startShow(){
+        console.log(`Starting the song ${this.song}`)
+    }
+    endShow(){
+        console.log('The show is ending')
+    }
+    bloopers(){
+        console.log('Here are some bloopers')
+    }
+}
