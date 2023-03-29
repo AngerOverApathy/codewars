e4d6c001cd4bf24097c99af2178a8a59db6b3b81
AngerOverApathy <violasian@gmail.com>
2023-03-29 10:48:02 -0700

    added frame

--- class27-materials/local-storage-botScore-button/js/main.js
+++ class27-materials/local-storage-botScore-button/js/main.js
@@ -3,7 +3,7 @@ localStorage.setItem('botScore', 0)
 document.querySelector('button').addEventListener('click', addAnothaOne)
 
 function addAnothaOne(){
-    let botScoreVal = localStorage.getItem('botscore')
+    let botScoreVal = Number(localStorage.getItem('botScore'))
     botScoreVal += 1
 
     localStorage.setItem('botScore', botScoreVal)
