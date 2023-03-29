dfc4190877b3d250853971fce27d0e8c58e6a7f0
AngerOverApathy <violasian@gmail.com>
2023-03-29 11:15:41 -0700

    card game

--- class27-materials/card-game/main.js
+++ class27-materials/card-game/main.js
@@ -1,26 +1,20 @@
-document.querySelector('button').addEventListener('click', getFetch)
+let deckId = ''
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-
-  const url = `https://api.nasa.gov/planetary/apod?api_key=JzpXPV9rPsbeHv95nKzwfsNA9FcgPrLVVt8AJKLx&date=${choice}`
-
-  fetch(url)
+fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-
-        if(data.media_type === 'image'){
-          document.querySelector('img').src = data.hdurl
-        } else if(data.media_type === 'video'){
-          document.querySelector('iframe').src = data.url
-          document.querySelector('img').src = null
-        }
-
-        document.querySelector('h3').innerText = data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
-}
+
+// document.querySelector('button').addEventListener('click', drawTwo)
+
+// function drawTwo(){
+//   const choice = document.querySelector('input').value
+
+//   const url = `https://api.nasa.gov/planetary/apod?api_key=JzpXPV9rPsbeHv95nKzwfsNA9FcgPrLVVt8AJKLx&date=${choice}`
+// }
+
 
