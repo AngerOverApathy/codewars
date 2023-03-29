4528ec567111b971fc51ef78ec6b60a10147a011
AngerOverApathy <violasian@gmail.com>
2023-03-29 12:06:56 -0700

    card game

--- class27-materials/card-game/main.js
+++ class27-materials/card-game/main.js
@@ -13,9 +13,17 @@ fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
 document.querySelector('button').addEventListener('click', drawTwo)
 
 function drawTwo(){
-const url = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`
+    const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
 
-
+    fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        deckId = data.deck_id
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
 }
 
 
