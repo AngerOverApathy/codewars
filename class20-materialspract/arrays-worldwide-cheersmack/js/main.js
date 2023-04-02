56ff0b3f30fde6761ac7082443e4eaf40d410f31
AngerOverApathy <violasian@gmail.com>
2023-04-02 15:45:58 -0700

    array practice

--- class20-materialspract/arrays-worldwide-cheersmack/js/main.js
+++
@@ -1,8 +0,0 @@
-//Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
-
-let arr = [1,2,3,4,5,6,7,8,9,10]
-
-function returnArr(num){
-    
-}
-
