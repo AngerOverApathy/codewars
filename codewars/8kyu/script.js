5ec0b555617bb9ee9478c9d860f1a4fca2b3ae86
AngerOverApathy <violasian@gmail.com>
2023-04-03 17:47:41 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -5,3 +5,17 @@
 //     },0)
 //     return sum
 //   }
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+// For example,
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+// function countSheeps(arrayOfSheep) {
+//     // TODO May the force be with you
+//       return arrayOfSheep.reduce((acc, sheep) => acc + sheep, 0)
+//   }
