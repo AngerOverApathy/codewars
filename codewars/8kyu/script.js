0be6ab06c33ca95729bcbfa57fa5cca5e1fa1d00
AngerOverApathy <violasian@gmail.com>
2023-04-03 18:28:55 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -19,3 +19,15 @@
 //     // TODO May the force be with you
 //       return arrayOfSheep.reduce((acc, sheep) => acc + sheep, 0)
 //   }
+
+// You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+// function positiveSum(arr) {
+//     let sum = arr.reduce((acc, num) => num > 0 ? acc+num : acc ,0)
+//     return sum
+//   }
+
+// function boolToWord( bool ){
+//     return bool ? 'Yes':'No';
+//   }
