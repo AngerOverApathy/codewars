c2246ee44fd7a5662f09698fd28bc59e055894ee
AngerOverApathy <violasian@gmail.com>
2023-04-03 18:55:03 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -31,3 +31,10 @@
 // function boolToWord( bool ){
 //     return bool ? 'Yes':'No';
 //   }
+
+// Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+// function solution(str){
+//     return str.split('').reverse().join('')
+//   }
