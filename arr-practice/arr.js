07566c32fc03ef703760e9d5eb514fd89857e1c3
AngerOverApathy <violasian@gmail.com>
2023-04-03 19:46:41 -0700

    codewars

--- arr-practice/arr.js
+++ arr-practice/arr.js
@@ -29,22 +29,22 @@
 // Use reduce() to sum the weights of all the cars
 // Stretch goal: 
 // Use reduce to sum the weights of only the electric cars 
-const cars = [
-    { name: "Toyota", isElectric: false, weight: 1320 },
-    { name: "Ford", isElectric: false, weight: 1400 },
-    { name: "Volkswagen", isElectric: false, weight: 1370 },
-    { name: "Honda", isElectric: false, weight: 1375 },
-    { name: "Tesla", isElectric: true, weight: 1750 },
-    { name: "BMW", isElectric: true, weight: 1350 },  
-  ];
-  const carTotals = cars.reduce((acc, car)=>{
-      return acc + car.weight
-  }, 0)
-  console.log(carTotals)
-  const electricCarTotals = cars.reduce((acc, car)=>{
-      if(car.isElectric){
-         acc += car.weight 
-      }
-      return acc
-  }, 0)
-  console.log(electricCarTotals)
+// const cars = [
+//     { name: "Toyota", isElectric: false, weight: 1320 },
+//     { name: "Ford", isElectric: false, weight: 1400 },
+//     { name: "Volkswagen", isElectric: false, weight: 1370 },
+//     { name: "Honda", isElectric: false, weight: 1375 },
+//     { name: "Tesla", isElectric: true, weight: 1750 },
+//     { name: "BMW", isElectric: true, weight: 1350 },  
+//   ];
+//   const carTotals = cars.reduce((acc, car)=>{
+//       return acc + car.weight
+//   }, 0)
+//   console.log(carTotals)
+//   const electricCarTotals = cars.reduce((acc, car)=>{
+//       if(car.isElectric){
+//          acc += car.weight 
+//       }
+//       return acc
+//   }, 0)
+//   console.log(electricCarTotals)
