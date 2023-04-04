b4b47674ce02105fa47d0dbbb0e4fe5afa4c497c
AngerOverApathy <violasian@gmail.com>
2023-04-03 17:47:22 -0700

    codewars

---
+++ codewars/8kyu/script.js
@@ -0,0 +1,7 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// function squareSum(numbers){
+//     let sum = numbers.reduce((acc, nums)=>{
+//       return acc += Math.pow(nums,2)
+//     },0)
+//     return sum
+//   }
