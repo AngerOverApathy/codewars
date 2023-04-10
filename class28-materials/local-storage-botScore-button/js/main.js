3e43e4376afc74477bd49e100b46a0166a09e540
AngerOverApathy <violasian@gmail.com>
2023-04-09 20:37:05 -0700

    codewars challenge

--- class28-materials/local-storage-botScore-button/js/main.js
+++ class28-materials/local-storage-botScore-button/js/main.js
@@ -6,11 +6,11 @@ if(!localStorage.getItem('botScore')){
 document.getElementById('title').innerText = localStorage.getItem('books')
 document.querySelector('button').addEventListener('click', getFetch)
 
-function anothaOne(){
-    let botScore = Number(localStorage.getItem('botScore'))
-    botScore = botScore + 1
-    localStorage.setItem('botScore', botScore)
-}
+// function anothaOne(){
+//     let botScore = Number(localStorage.getItem('botScore'))
+//     botScore = botScore + 1
+//     localStorage.setItem('botScore', botScore)
+// }
 
 function getFetch(){
     anothaOne();
