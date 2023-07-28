ffcdabb48256aed799a713d7eac3f33fb0932cd5
AngerOverApathy <violasian@gmail.com>
2023-07-28 10:57:30 -0700

    codewars

--- codewars/7kyu/script.js
+++ codewars/7kyu/script.js
@@ -2,15 +2,15 @@
 // Examples:
 // 1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
 // 4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
-function dontGiveMeFive(start, end) {
-    let count = 0;
-    for (let i = start; i <= end; i++) {
-      if (!i.toString().includes('5')) {
-        count++;
-      }
-    }
-    return count;
-  }
+// function dontGiveMeFive(start, end) {
+//     let count = 0;
+//     for (let i = start; i <= end; i++) {
+//       if (!i.toString().includes('5')) {
+//         count++;
+//       }
+//     }
+//     return count;
+//   }
 
 
 
