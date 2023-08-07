13e5ed0498b76829db4e4db83f4a1f2b2a41e00d
AngerOverApathy <violasian@gmail.com>
2023-08-07 16:44:05 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -134,7 +134,7 @@
 //     return `found the needle at position ${index}`
 //   }
 //OR
-function findNeedle(haystack) {
-    return "found the needle at position " + haystack.indexOf("needle");
-  }
+// function findNeedle(haystack) {
+//     return "found the needle at position " + haystack.indexOf("needle");
+//   }
 
