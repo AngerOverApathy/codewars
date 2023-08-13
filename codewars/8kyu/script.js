e0a7505ca1b3638d193c01c58015f67c20873e50
AngerOverApathy <violasian@gmail.com>
2023-08-13 16:36:47 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -115,12 +115,12 @@
 
 // Write a function which calculates the average of the numbers in a given list.
 // Note: Empty arrays should return 0.
-function findAverage(array) {
-    if (array === null || array.length === 0){
-      return 0
-    } 
-    const sum = array.reduce((acc, num) => num += acc, 0)
-    return sum / array.length
-  }
+// function findAverage(array) {
+//     if (array === null || array.length === 0){
+//       return 0
+//     } 
+//     const sum = array.reduce((acc, num) => num += acc, 0)
+//     return sum / array.length
+//   }
 
 
