82616fb28b8694786af015465ae51c92b05580f9
AngerOverApathy <violasian@gmail.com>
2023-09-07 15:51:07 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -52,9 +52,9 @@
 //   }
 
 //Complete the square sum function so that it squares each number passed into it and then sums the results together.
-function squareSum(numbers){
-    return numbers.reduce((acc, num) => acc + (num * num), 0);
-  }
+// function squareSum(numbers){
+//     return numbers.reduce((acc, num) => acc + (num * num), 0);
+//   }
 
 
 
