3f6c31cbc991036776b14ac8739d57545a2bbde9
AngerOverApathy <violasian@gmail.com>
2023-09-12 15:39:06 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -38,6 +38,16 @@
 //     return array.map(num => num * -1)
 //  }
 
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+// For example,
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
 
 
 
