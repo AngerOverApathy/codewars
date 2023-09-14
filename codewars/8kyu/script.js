0ed8b35d7b307a8011c185c6f61be83820cd7b5b
AngerOverApathy <violasian@gmail.com>
2023-09-14 15:34:48 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -34,7 +34,9 @@
 // };
 
 //Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
-
+// function invert(array) {
+//     return array.map(num => num * -1)
+//  }
 
 
 
