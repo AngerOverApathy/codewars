b462049b2ceb521ae6b66458198c27d6015a4509
AngerOverApathy <violasian@gmail.com>
2023-09-16 15:31:20 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -24,14 +24,14 @@
 //   }
 
 //Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
-function sum (numbers) {
-    "use strict";
-    if (numbers === null || numbers.length === 0) {
-      return 0
-    }
-  return numbers.reduce((acc, num)=> num += acc, 0)
+// function sum (numbers) {
+//     "use strict";
+//     if (numbers === null || numbers.length === 0) {
+//       return 0
+//     }
+//   return numbers.reduce((acc, num)=> num += acc, 0)
     
-};
+// };
 
 
 
