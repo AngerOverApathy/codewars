2eafbc7a946e73e66aaf9a2edca1d0d034513d1b
AngerOverApathy <violasian@gmail.com>
2023-09-24 09:52:36 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -230,3 +230,12 @@
 // Given an array of integers.
 // Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
 // If the input is an empty array or is null, return an empty array.
+// function countPositivesSumNegatives(input) {
+//     if (input === null || input.length === 0) {
+//           return [];
+//       }
+//       return input.reduce((accumulator, num) => {
+//           num > 0 ? accumulator[0]++ : num < 0 ? accumulator[1] += num : null;
+//           return accumulator;
+//       }, [0, 0])
+//   }
