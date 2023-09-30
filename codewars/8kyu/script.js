e9b36323fe8deb49356a19a8b0a5a06da7ad31a1
AngerOverApathy <violasian@gmail.com>
2023-09-30 09:18:15 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -211,3 +211,8 @@
 //     }
 //         return multiples;  // Return the list of multiples
 // }
+
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+// Examples (Input -> Output)
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
