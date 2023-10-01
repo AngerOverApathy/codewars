43fcdd08f5f020279d6f0e71139c9855ed46e911
AngerOverApathy <violasian@gmail.com>
2023-10-01 09:17:30 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -203,3 +203,11 @@
 
 //In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
 //For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+//function findMultiples(integer, limit) {
+//     let multiples = [];  // An array to store the multiples
+//         // Iterate from the integer up to and including the limit
+//     for (let i = integer; i <= limit; i += integer) {
+//         multiples.push(i);  // If i is a multiple of the integer, add it to the list
+//     }
+//         return multiples;  // Return the list of multiples
+// }
