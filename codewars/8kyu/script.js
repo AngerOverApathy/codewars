b747e020debfc9ffda6cdeb4c4bdb8390141a1cd
AngerOverApathy <violasian@gmail.com>
2023-10-13 21:29:14 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -178,6 +178,6 @@
 //   }
 
 //Very simple, given an integer or a floating-point number, find its opposite.
-function opposite(number) {
-    return -(number)
-  }
+// function opposite(number) {
+//     return -(number)
+//   }
