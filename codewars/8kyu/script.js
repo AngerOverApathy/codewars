be6d5ff5a48bba68f76d922b1444faccce4013ab
AngerOverApathy <violasian@gmail.com>
2023-10-21 21:24:56 -0700

    codewars

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -19,9 +19,9 @@
 // Notes
 // The number can be negative already, in which case no change is required.
 // Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
-function makeNegative(num) {
-    return -Math.abs(num)
-  }
+// function makeNegative(num) {
+//     return -Math.abs(num)
+//   }
 
 
 
