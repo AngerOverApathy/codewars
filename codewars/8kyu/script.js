a3a0fdb2fc8eaf889254ef729e3f7056e2d074d4
AngerOverApathy <violasian@gmail.com>
2023-10-27 21:17:50 -0700

    update

--- codewars/8kyu/script.js
+++ codewars/8kyu/script.js
@@ -5,6 +5,8 @@
 //     return arr.reduce((acc,num)=> acc + (num > 0 ? num : 0),0);
 //  }
 
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
 
 
 
