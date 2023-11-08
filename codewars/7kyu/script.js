696d0252c0e5ab68867079c71c67b3840cb7744c
AngerOverApathy <violasian@gmail.com>
2023-11-08 13:39:05 -0800

    codewars

--- codewars/7kyu/script.js
+++ codewars/7kyu/script.js
@@ -33,3 +33,17 @@
 // function minMax(arr){
 //     return [Math.min(...arr), Math.max(...arr)];
 //   }
+
+// The museum of incredible dull things
+// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+// Task
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+// Don't change the order of the elements that are left.
+// function removeSmallest(numbers) {
+//     let arrCopy = numbers.slice()
+//     let smallestNum = Math.min(...arrCopy)
+//     let smallestNumIdx = arrCopy.indexOf(smallestNum)
+//     arrCopy.splice(smallestNumIdx, 1)
+//     return arrCopy
+//   }
